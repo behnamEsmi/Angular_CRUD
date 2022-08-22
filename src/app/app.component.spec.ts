611e2576a29bea069a00cb790cef0a91988b9654
behnamesmi <behnam.esmi.dev@gmail.com>
2022-08-22 04:55:52 +0430

    create_addedit_component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular_CRUD'`, () => {
+  it(`should have as title 'angular-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular_CRUD');
+    expect(app.title).toEqual('angular-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular_CRUD app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-project app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import  {HttpClientModule} from '@angular/common/http';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InspectionComponent } from './inspection/inspection.component';
+import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add-edit-inspection.component';
+import { ShowInspectionComponent } from './inspection/show-inspection/show-inspection.component';
+import { InsectionApiService } from './insection-api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InspectionComponent,
+    AddEditInspectionComponent,
+    ShowInspectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [InsectionApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
